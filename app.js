7dae1a7f6b1cd9797c9f8d89dae48245206b2ef1
Goodbear1997 <93277547+Goodbear1997@users.noreply.github.com>
2022-01-19 20:52:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+$(function () {
+    let slider = $("#slider");
+    slider.slick({
+        infinite: true,
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         fade: false,
+         arrows: false,
+        dots:true
+    });
+});
+
